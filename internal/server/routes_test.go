df3a058135011827f1c017cbcde18255e9665cee
Tran Nhat Tien <nhattientran284@gmail.com>
2024-09-26 16:56:04 +0700

    update unit test

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -25,7 +25,7 @@ func TestHelloWorldHandler(t *testing.T) {
 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 	// Check the response body
-	expected := "{\"message\":\"Hello World\"}"
+	expected := "{\"environment\":\"\",\"message\":\"healthy\",\"version\":\"1.0.0\"}"
 	if rr.Body.String() != expected {
 		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
